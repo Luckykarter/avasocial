1687f564ff12d0e17b8590bb1063378c1833db4c
Egor Wexler <ferotex7@gmail.com>
2021-02-07 22:22:06 +0300

    Force SSL

--- avasocial/settings.py
+++ avasocial/settings.py
@@ -65,7 +65,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'avasocial.urls'
 
